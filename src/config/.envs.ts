35f06daf9756dfa80c0641ec3be07a50f2828bb3
Nicolay <nicolaymorenoherrera@gmail.com>
2024-09-19 17:16:17 -0500

    Nats added

--- src/config/.envs.ts
+++ src/config/.envs.ts
@@ -5,14 +5,23 @@ interface EnvVars {
     PORT: number;
     DATABASE_URL: string;
 
+    NATS_SERVERS: string[];
 }
 
 const envVarsSchema = joi.object({
     PORT: process.env.PORT,
+    DATABASE_URL: joi.string().required(),
+
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
 })
 .unknown(true);
 
-const { error, value } = envVarsSchema.validate( process.env );
+const { error, value } = envVarsSchema.validate({
+
+    ...process.env,
+    NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+
+});
 
 if ( error ) {
     throw new Error(`Config validation error: ${ error.message }`);
@@ -23,6 +32,8 @@ const envVars: EnvVars = value;
 export const envs = {
     port: value.PORT,
     databaseUrl: value.DATABASE_URL,
+
+    natsServers: envVars.NATS_SERVERS,
 }
 
 
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
+import { envs } from './config/.envs';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
-import { envs } from './config/.envs';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
 
   const logger = new Logger('Main');
 
+  console.log(envs.natsServers);
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      transport: Transport.TCP,
+      transport: Transport.NATS,
       options: {
-        port: envs.port,
+        servers: envs.natsServers,
       }
     }
   );
